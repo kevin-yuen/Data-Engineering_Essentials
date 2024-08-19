8c3f3c8ce5bc3d9a312f208e7d0add7cd9cd9ef2
kevin-yuen <kevin.yuen11@gmail.com>
2024-08-18 22:57:37 -0400

    moved connect-to-db-practice.py

---
+++ connect-to-db-practice.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sqlalchemy as sa
+import psycopg2 as pc
+
+conn_uri = 'postgresql://postgres:T%40lipz123@localhost:5432/itversity_retail_db'
+#
+
+output = pd.read_sql('orders', conn_uri)
+print(output)
